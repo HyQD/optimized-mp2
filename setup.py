9c3731f63ad73995d3d2d1ebfee6cc2a16c987a9
haakoek <haakoek@student.matnat.uio.no>
2021-06-01 17:40:37 +0200

    add license, setup and environment files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Optimized MP2",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "scipy",
+        "opt_einsum",
+        "quantum-systems @ git+https://github.com/Schoyen/quantum-systems",
+    ],
+)
